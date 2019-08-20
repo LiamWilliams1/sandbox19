e2fe9228b78c9d425300f6777dfccc20ef7a2385
LiamWilliams1 <liam.williams@my.jcu.edu.au>
2019-08-20 16:32:11 +1000

    add password checker

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,8 @@
 """Liam Williams"""
+
+MINIMUM_LENGTH = 4
+password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+while len(password) < MINIMUM_LENGTH:
+    password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+
+print('*' * len(password))
